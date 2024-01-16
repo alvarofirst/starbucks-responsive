9f8134b58184d9c35e31d9256dad16db45fe78d1
alvarofirst <noexistuser16@gmail.com>
2024-01-16 17:22:25 -0500

    js changed

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 let btnMenu = document.querySelector(".menu");
 let arrowDown = document.querySelectorAll(".accordion");
-let width = screen.width;
 
-arrowDown.forEach((arrow) => {
-  arrow.addEventListener("click", () => {
-    let list = arrow.nextElementSibling;
-    let containClass = arrow.nextElementSibling.classList.contains("visible");
-    if (containClass && width < 1024) {
-      list.classList.remove("visible");
-      list.style.display = "block";
-      return;
-    } else if (!containClass && width < 1024) {
-      list.classList.add("visible");
-      list.style.display = "none";
+function handleClick() {
+  arrowDown.forEach((arrow) => {
+    arrow.addEventListener("click", () => {
+      let list = arrow.nextElementSibling;
+      list.style.display = list.classList.contains("visible")
+        ? "block"
+        : "none";
+      list.classList.toggle("visible");
+    });
+  });
+}
+
+if (window.innerWidth < 1024) {
+  handleClick();
+  window.addEventListener("resize", () => {
+    if (window.innerWidth < 1024) {
+      handleClick();
     }
   });
-});
+}
